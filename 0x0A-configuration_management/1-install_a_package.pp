97a5ef4dc3292cb80f8f51f88739c779abdfb34e
davenyam <itsdavie2014@gmail.com>
2024-08-25 15:29:16 +0300

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 #install flask from pip3
 
-install {'flask':
-  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
+exec { 'install flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
